f27bbd14b1356fcef492ea9b1afebf12b198a74c
Arman Ebrahimi <armanebrhm@gmail.com>
2021-02-17 12:38:08 +0100

    first commit

---
+++ src/main/java/com/example/coronatrackerapp/CoronaTrackerAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.coronatrackerapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoronaTrackerAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoronaTrackerAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/coronatrackerapp/CoronaTrackerAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.coronatrackerapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoronaTrackerAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
